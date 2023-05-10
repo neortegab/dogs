aad20e1b7f6be7d37fbc0c94f5203fc5599b59ea
Nelson Ortega <ne.beltran18@gmail.com>
2023-05-10 17:31:33 -0500

    Navbar update to only render search bar at Home

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import NavbarButton from './AuxButtons/NavbarButton';
 import home from '../images/home.svg';
 import paw from '../images/paw.svg';
 import './Styles/Navbar.css'
 
 export default function Navbar() {
+
+  const { pathname } = useLocation();
+
   return (
     <div className='navbar-container'>
-        <NavbarButton action={'Home'} image={home} />
-        <NavbarButton action={'Temperaments'} image={paw} />
-        <button className='navbar-button'>Search</button>
-        <input className='navbar-input' type="text" placeholder="German shepard" />
+        <Link to='/home'>
+          <NavbarButton action={'Home'} image={home} />
+        </Link>
+        <Link to='/temperaments'>
+          <NavbarButton action={'Temperaments'} image={paw} />
+        </Link>
+        { pathname === '/home' && 
+          <div className='navbar-searchbar'> 
+            <button className='navbar-button'>Search</button>
+            <input className='navbar-input' type="text" placeholder="German shepard" /> 
+          </div> 
+        }
     </div>
   )
 }
