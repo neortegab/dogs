69d65a7f7759a6d2e51bbecaa0ee77252d142ede
Nelson Ortega <ne.beltran18@gmail.com>
2023-05-11 21:43:46 -0500

    Action button to reset filters made

--- client/src/Components/AuxButtons/ActionButton.jsx
+++ client/src/Components/AuxButtons/ActionButton.jsx
@@ -3,9 +3,9 @@ import paw from '../../images/paw.svg'
 import '../Styles/ActionButton.css'
 
 export default function ActionButton(props) {
-    const { action } = props;
+    const { action, onClick } = props;
   return (
-    <div className='action-button'>
+    <div className='action-button' onClick={onClick}>
         <p className='action-name'>{action}</p>
         <img className='action-image' src={paw} alt={action}/>
     </div>
--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { clearFilter } from '../redux/actions/actions'
 import SidebarOption from './AuxComponents/SidebarOption'
 import ActionButton from './AuxButtons/ActionButton'
 import './Styles/Sidebar.css'
 
 export default function Sidebar() {
+
+  const { pathname } = useLocation();
+
+  const dispatch = useDispatch();
+
+  const handleReset = () => {
+    dispatch(clearFilter());
+  } 
+
   return (
     <div className='sidebar-container'>
         <h1 className='sidebar-title'>Options</h1>
         <h2 className='sidebar-subtitle'>Sort</h2>
         <SidebarOption name='Alphabetically'/>
-        <SidebarOption name='Weight'/> 
+        { pathname !== '/temperaments' && <SidebarOption name='Weight'/> }
+        <div className='sidebar-reset-button'>
+          <ActionButton action='Reset' onClick={handleReset}/>
+        </div>
         <h2 className='sidebar-subtitle'>Filters</h2>
         <div>
           <select>
@@ -18,9 +33,7 @@ export default function Sidebar() {
         </div>
         <h2 className='sidebar-subtitle'>List actions</h2>
         <div className='sidebar-buttons-list'>
-            <ActionButton action='Create'/>
-            <ActionButton action='Update'/>
-            <ActionButton action='Delete'/>
+            <ActionButton action='Add'/>
         </div>
     </div>
   )
